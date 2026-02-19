e8b4be0e4470d07015d9fb426e77c5600bf412cf
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2026-02-19 14:33:01 +0530

    Chat Get Create

--- backend/src/controllers/chatController.ts
+++ backend/src/controllers/chatController.ts
@@ -2,6 +2,7 @@ import type { Request, Response, NextFunction } from "express";
 import { Chat } from "../models/Chat";
 import { Message } from "../models/Message";
 import type { AuthRequest } from "../middleware/auth";
+import mongoose from "mongoose";
 
 export async function getChats(
   req: AuthRequest,
@@ -35,11 +36,41 @@ export async function getChats(
   }
 }
 export async function getChatOrCreate(
-  req: Request,
+  req: AuthRequest,
   res: Response,
   next: NextFunction,
 ) {
   try {
+    const userId = new mongoose.Types.ObjectId(req.userId);
+    const participantId = new mongoose.Types.ObjectId(req.params.participantId);
+
+    let chat = await Chat.findOne({
+      participantsId: { $all: [userId, participantId] },
+    })
+      .populate("participantsId", "username email avatar")
+      .populate("lastMessageId");
+
+    if (!chat) {
+      chat = await Chat.create({
+        participantsId: [userId, participantId],
+      });
+
+      chat = await chat.populate("participantsId", "username email avatar");
+    }
+    const receiveParticipants = chat.participantsId.filter(
+      (participant) => participant._id.toString() !== userId.toString(),
+    );
+    const obj = {
+      chatId: chat._id.toString(),
+      participants: receiveParticipants ?? null,
+      lastMessage: chat.lastMessageId,
+      lastMessageAt: chat.lastMessageAt,
+      createdAt: chat.createdAt,
+    };
+    return res.status(200).json({
+      success: true,
+      data: obj,
+    });
   } catch (error) {
     next(error);
   }
